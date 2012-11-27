08f97e04ae24f5c9781866beb5f850991ff3e5d8
Charles <cam213@case.edu>
2012-11-27 11:38:20 -0500

    Added addToDate, subtractFromDate, daysToNextInstanceOf, and daysSinceLastInstanceOf.
    Signed-off-by: Charles <cam213@case.edu>

--- src/Tzolkin.java
+++ src/Tzolkin.java
@@ -28,7 +28,24 @@ public class Tzolkin {
 	 */
 	public int subtractDates(Tzolkin t)
 	{
+		if(this.equals(t))
+		{
+			return 0;
+		}
 		
+		//int numDiff = Math.abs(this.number - t.number);
+		int daysToNext = daysToNextInstanceOf(t);
+		int daysSinceLast = daysSinceLastInstanceOf(t);
+		
+		if(daysToNext < daysSinceLast)
+		{
+			return daysToNext;
+		}
+		else if(daysSinceLast < daysToNext)
+		{
+			return daysSinceLast;
+		}
+		return -1;
 	}
 	
 	/**
@@ -41,12 +58,26 @@ public class Tzolkin {
 	public Tzolkin addToDate(int days)
 	{
 		//The number of the future day
-		int futureNumber = ((this.number + 1 + days) % 13) - 1;
+		int futureNumber = ((this.number + days) % 13);
 		//The index in names of the name of the future day
 		int futureNameNum = ((this.nameNum + 1 + days) % 20) - 1;
 		String futureName = names[futureNameNum];
 		
 		return new Tzolkin(futureNumber, futureName);
+	}
+	
+	/**
+	 * 
+	 * @param days
+	 * @return
+	 */
+	public Tzolkin subtractFromDate(int days)
+	{
+		int pastNumber = (this.number + (13 - days))%13;
+		int pastNameNum = ((this.nameNum + 1 + (20 - days))%20) - 1;
+		String pastName = names[pastNameNum];
+		
+		return new Tzolkin(pastNumber, pastName);
 	}
 	
 	/**
@@ -67,4 +98,62 @@ public class Tzolkin {
 		}
 		return -1;
 	}
+	
+	/**
+	 * Finds the number of days to the next instance of t from this
+	 * 
+	 * @param t
+	 * @return
+	 */
+	private int daysToNextInstanceOf(Tzolkin t)
+	{
+		//this == 2.Manik && t == 9.Imix
+		Tzolkin temp = t;
+		int daysToNextInstance = 0;
+		int numberDiff = Math.abs(this.number - t.number);	//numberDiff = 7
+		daysToNextInstance += numberDiff;	//daysToNext = 7
+		temp = temp.addToDate(numberDiff);	//temp = 9.Ix
+		while(!this.equals(temp))
+		{
+			temp = temp.addToDate(13);
+			daysToNextInstance += 13;
+		}
+		return daysToNextInstance;
+	}
+	
+	/**
+	 * 
+	 * @param t
+	 * @return
+	 */
+	private int daysSinceLastInstanceOf(Tzolkin t)
+	{
+		//this == 2.Manik && t == 9.Imix
+		Tzolkin temp = t;
+		int daysSinceLastInstance = 0;
+		int numberDiff = 13 - Math.abs(this.number - t.number);
+		daysSinceLastInstance += numberDiff;
+		temp = temp.subtractFromDate(numberDiff);
+		while(!this.equals(temp))
+		{
+			temp = temp.subtractFromDate(13);
+			daysSinceLastInstance += 13;
+		}
+		return daysSinceLastInstance;
+	}
+	
+	/**
+	 * If this and t are equal, returns true, else returns false.
+	 * 
+	 * @param t Tzolkin to be compared to this
+	 * @return True if equal, else false
+	 */
+	private boolean equals(Tzolkin t)
+	{
+		return (this.name == t.name
+				&& this.number == t.number
+				&& this.nameNum == t.nameNum);
+	}
+	
+	
 }
